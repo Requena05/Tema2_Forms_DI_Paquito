a1d9239a7ecd76423949a849d930b6a872ed6a3b
PACO <franreke506@gmail.com>
2024-10-31 19:07:51 +0100

    Ejercicio 3 Formularios - Añadida la validación de campos para correo, nombre y contraseña. - Añadido un selector de fecha con validación de edad. - Añadido un botón que solo se muestra si los campos son válidos. - La actividad E3 se configura como la principal en el manifiesto.

--- app/src/main/java/com/example/tema2_forms_di_paquito/E3Activity.kt
+++ app/src/main/java/com/example/tema2_forms_di_paquito/E3Activity.kt
@@ -1,41 +1,34 @@
-package com.example.tema2_forms_di_paquito;
+package com.example.tema2_forms_di_paquito
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.toColorInt
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.doOnTextChanged
-import androidx.transition.Visibility
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-class E2Activity : AppCompatActivity() {
-
+class E3Activity : AppCompatActivity() {
     private lateinit var nombre: TextInputEditText
     private lateinit var layoutNombre: TextInputLayout
     private lateinit var boton: Button
     private lateinit var campocorreo: TextInputLayout
     private lateinit var correo: TextInputEditText
-    private lateinit var campocontraseña:TextInputLayout
-    private lateinit var contraseña:TextInputEditText
+    private lateinit var campocontraseña: TextInputLayout
+    private lateinit var contraseña: TextInputEditText
     private var esnombrevalid : Boolean=false
     private var escorreovalid : Boolean=false
     private var escontraseñavalid : Boolean=false
-    @SuppressLint("UseCompatLoadingForDrawables", "NewApi")
+    private lateinit var fecha: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-
-        setContentView(R.layout.activity_e2)
-
+        setContentView(R.layout.activity_e3)
         layoutNombre=findViewById(R.id.camponombre)
         nombre=findViewById(R.id.nombre)
         correo=findViewById(R.id.correo)
@@ -43,8 +36,28 @@ class E2Activity : AppCompatActivity() {
         boton = findViewById(R.id.insertar)
         campocontraseña=findViewById(R.id.campocontraseña)
         contraseña=findViewById(R.id.contraseña)
-        boton.visibility= View.GONE
-            nombre.doOnTextChanged { text, start, count, after ->
+        fecha=findViewById(R.id.fecha)
+
+        fecha.setOnClickListener{
+            val buider= MaterialDatePicker.Builder.datePicker()
+            val modalQueSeAbre=buider.build()
+            modalQueSeAbre.show(supportFragmentManager,"DATE_PICKER")
+            modalQueSeAbre.addOnPositiveButtonClickListener {
+                fecha.setText(modalQueSeAbre.headerText)
+
+                //el usuario no puede ser menor de edad
+            if(modalQueSeAbre.selection!! > System.currentTimeMillis() - 315360000000  ){
+                fecha.error="Fecha no valida"
+            }else{
+                fecha.error=null
+            }
+            }
+
+        }
+
+
+
+        nombre.doOnTextChanged { text, start, count, after ->
 
 
             if (nombre.text.toString().length < 2) {
@@ -89,50 +102,34 @@ class E2Activity : AppCompatActivity() {
                 comprobarForm()
             }
         }
-            contraseña.doOnTextChanged { text, start, count, after ->
-                if (contraseña.text.toString().length > 6) {
-                    escontraseñavalid=true
-                    campocontraseña.boxStrokeColor = getColor(R.color.verde)
-                    campocontraseña.hintTextColor = getColorStateList(R.color.verde)
-                    campocontraseña.helperText = "contraseña valida"
-                    campocontraseña.hintTextColor = getColorStateList(R.color.verde)
-                    campocontraseña.setHelperTextColor(getColorStateList(R.color.verde))
-                    campocontraseña.setEndIconActivated(true)
-                    campocontraseña.setEndIconDrawable(R.drawable.baseline_check_box_24)
-                    campocontraseña.setEndIconTintList(getColorStateList(R.color.verde))
-                    comprobarForm()
+        contraseña.doOnTextChanged { text, start, count, after ->
+            if (contraseña.text.toString().length > 6) {
+                escontraseñavalid=true
+                campocontraseña.boxStrokeColor = getColor(R.color.verde)
+                campocontraseña.hintTextColor = getColorStateList(R.color.verde)
+                campocontraseña.helperText = "contraseña valida"
+                campocontraseña.hintTextColor = getColorStateList(R.color.verde)
+                campocontraseña.setHelperTextColor(getColorStateList(R.color.verde))
+                campocontraseña.setEndIconActivated(true)
+                campocontraseña.setEndIconDrawable(R.drawable.baseline_check_box_24)
+                campocontraseña.setEndIconTintList(getColorStateList(R.color.verde))
+                comprobarForm()
 
 
 
-                } else {
-                    campocontraseña.error = "Contraseña no permitida"
-                    escontraseñavalid=false
-                    comprobarForm()
-                }
+            } else {
+                campocontraseña.error = "Contraseña no permitida"
+                escontraseñavalid=false
+                comprobarForm()
             }
-
-
-        boton.setOnClickListener{Log.d("E2Activity", "Boton pulsado")
-
-            nombre.text?.clear()
-            correo.text?.clear()
-            contraseña.text?.clear()
-            layoutNombre.error=null
-            campocontraseña.error=null
-            campocorreo.error=null
-            nombre.focusable=View.NOT_FOCUSABLE
-            correo.focusable=View.NOT_FOCUSABLE
-            contraseña.focusable=View.NOT_FOCUSABLE
-
         }
 
 
     }
-
     private fun comprobarForm() {
         if (escorreovalid && esnombrevalid && escontraseñavalid) {
             boton.visibility = View.VISIBLE
         }else
-            boton.visibility=View.GONE
+            boton.visibility= View.GONE
     }
 }
